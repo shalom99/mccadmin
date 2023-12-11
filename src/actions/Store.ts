4e9c1348135de292d99b77d448e5f791ebbf47e7
shalom99 <castroshalom77@gmail.com>
2023-12-12 07:57:40 +0800

    latest dialog

--- src/actions/Store.ts
+++ src/actions/Store.ts
@@ -3,7 +3,27 @@
 import { prisma } from "@/lib/prismadb"
 
 export async function getProducts() {
-    const getProducts = await prisma.products.findMany()
+    const products = await prisma.products.findMany()
 
-    return getProducts;
+    return products;
+}
+
+export async function updateProduct(newProductData: any){
+
+
+
+    const setUppdateProduct = await prisma.products.update({
+        where: {
+            product_id: newProductData.product_id
+        },
+        data: {
+            product_name: newProductData.product_name,
+            description: newProductData.description,
+            price: newProductData.price,
+            quantity_in_stock: newProductData.quantity_in_stock,
+            image_url: newProductData.image_url,
+        }
+    })
+
+
 }
--- src/app/products/components/ProductsTable.tsx
+++ src/app/products/components/ProductsTable.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FC } from "react";
+import { FC, useState } from "react";
 
 import { MdModeEdit, MdDeleteOutline } from "react-icons/md";
 import {
@@ -11,55 +11,103 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import EditProductDialog from "./EditProductDialog";
+
+type productType = {
+  product_id: number,
+  product_name: string,
+  description: string,
+  price: number,
+  quantity_in_stock: number,
+  category_id: number,
+  image_url: string,
+}
+
 type ProductsTableProps = {
   products: any;
 };
 
+
+
 const ProductsTable: FC<ProductsTableProps> = ({ products }) => {
-  console.log(products[0]);
+//   console.log(products[0]);
+  const [selectedProduct, setSelectedProduct ] = useState(products[0])
+  const [showDialog, setShowDialog] = useState(true) 
+    console.log(selectedProduct)
   return (
-    <Table>
-      <TableCaption>A list of products in store</TableCaption>
-      <TableHeader>
-        <TableRow>
-        <TableHead className=" flex gap-x-2 items-center justify-start">
-        <input type="checkbox" />
-          Select All</TableHead>
-          <TableHead className="w-[100px]">Item #</TableHead>
-          <TableHead>Name</TableHead>
-          <TableHead>Description</TableHead>
-          <TableHead className="text-right">Price</TableHead>
-          <TableHead className="text-center">Quantity in stock</TableHead>
-          <TableHead className="text-right">Image</TableHead>
-          <TableHead className="text-center">Actions</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {products.map((item: any, index: number) => (
-          <>
-            <TableRow>
-            <TableCell>
+    <Dialog>
+      <Table>
+        <TableCaption>A list of products in store</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className=" flex gap-x-2 items-center justify-start">
               <input type="checkbox" />
-              </TableCell>
-              <TableCell className="font-medium">{index + 1}</TableCell>
-              <TableCell>{item.product_name}</TableCell>
-              <TableCell>{item.description}</TableCell>
-              <TableCell className="text-right">{item.price}</TableCell>
-              <TableCell className="text-center">
-                {item.quantity_in_stock}
-              </TableCell>
-              <TableCell className="text-right">{item.image_url}</TableCell>
-              <TableCell className="">
-                <div className="flex items-center justify-center gap-x-2">
-                  <MdModeEdit size="25"/>
-                  <MdDeleteOutline size="25"/>
-                </div>
-              </TableCell>
-            </TableRow>
-          </>
-        ))}
-      </TableBody>
-    </Table>
+              Select All
+            </TableHead>
+            <TableHead className="w-[100px]">Item #</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Description</TableHead>
+            <TableHead className="text-right">Price</TableHead>
+            <TableHead className="text-center">Quantity in stock</TableHead>
+            <TableHead className="text-right">Image</TableHead>
+            <TableHead className="text-center">Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {products.map((item: productType, index: number) => (
+            <>
+              <TableRow>
+                <TableCell>
+                  <input type="checkbox" />
+                </TableCell>
+                <TableCell className="font-medium">{index + 1}</TableCell>
+                <TableCell>{item.product_name}</TableCell>
+                <TableCell>{item.description}</TableCell>
+                <TableCell className="text-right">${item.price}</TableCell>
+                <TableCell className="text-center">
+                  {item.quantity_in_stock}
+                </TableCell>
+                <TableCell className="text-right">{item.image_url}</TableCell>
+                <TableCell className="">
+                  <div className="flex items-center justify-center gap-x-2">
+                    <button className="" onClick={(e) => {setShowDialog(true); setSelectedProduct(item)}}>
+                    <MdModeEdit size="25" />
+                    </button>
+
+                    <MdDeleteOutline size="25" />
+                  </div>
+                </TableCell>
+              </TableRow>
+            </>
+          ))}
+        </TableBody>
+      </Table>
+
+      {showDialog && <EditProductDialog product={selectedProduct} setShowDialog={setShowDialog} setSelectedProduct={setSelectedProduct}/>}
+               
+            <p>{JSON.stringify(selectedProduct)}</p>
+
+      <DialogTrigger>open</DialogTrigger>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 };
 
