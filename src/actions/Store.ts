d842652c209e5a28879d81df5b3014c7c3586fb6
shalom99 <castroshalom77@gmail.com>
2024-01-19 16:57:56 +0800

    test revalidate

--- src/actions/Store.ts
+++ src/actions/Store.ts
@@ -1,66 +1,56 @@
-'use server'
+"use server";
 
-import { prisma } from "@/lib/prismadb"
+import { prisma } from "@/lib/prismadb";
 import { revalidatePath } from "next/cache";
 
 
-  function convertPricesToString(productsArray:any) {
-    for (let i = 0; i < productsArray.length; i++) {
-      productsArray[i].price = productsArray[i].price.toFixed(2);
-    }
-    return productsArray;
+
+function convertPricesToString(productsArray: any) {
+  for (let i = 0; i < productsArray.length; i++) {
+    productsArray[i].price = productsArray[i].price.toFixed(2);
   }
+  return productsArray;
+}
 
 export async function getProducts() {
-    const products = await prisma.products.findMany()
+  const products = await prisma.products.findMany();
 
-    const productsWithStringPrices = convertPricesToString(products);
-console.log(productsWithStringPrices);
+  const productsWithStringPrices = convertPricesToString(products);
+  console.log(productsWithStringPrices);
 
-    return productsWithStringPrices;
+  return productsWithStringPrices;
 }
 
+export async function updateProduct(newProductData: any) {
+  const product = await prisma.products.findUnique({
+    where: {
+      product_id: newProductData.product_id,
+    },
+  });
 
-
-export async function updateProduct(newProductData: any){
-
-    const product = await prisma.products.findUnique({
-        where: {
-            product_id: newProductData.product_id
-        }
-    })
-
-
-    const setUppdateProduct = await prisma.products.update({
-        where: {
-            product_id: newProductData.product_id
-        },
-        data: {
-            product_name: newProductData.product_name,
-            description: newProductData.description,
-            price: newProductData.price,
-            quantity_in_stock: newProductData.quantity_in_stock,
-            image_url: newProductData.image_url,
-        }
-    })
-
-
+  const setUppdateProduct = await prisma.products.update({
+    where: {
+      product_id: newProductData.product_id,
+    },
+    data: {
+      product_name: newProductData.product_name,
+      description: newProductData.description,
+      price: newProductData.price,
+      quantity_in_stock: newProductData.quantity_in_stock,
+      image_url: newProductData.image_url,
+    },
+  });
 }
 
-export async function deleteProduct(productId: number){
-
-    try{
-        await prisma.products.delete({
-            where: {
-                product_id:  productId
-            }
-        })
-
-        revalidatePath('/products')
-    }catch(err){
-        console.log("Error Deleting")
-    }
-
-
+export async function deleteProduct(productId: number) {
+  try {
+    await prisma.products.delete({
+      where: {
+        product_id: productId,
+      },
+    });
+    revalidatePath("/products");
+  } catch (err) {
+    console.log("Error Deleting");
+  }
 }
-
